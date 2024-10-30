b72ac069f499fe3517c7f1d5776b79bfaab01cbd
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-10-30 17:29:29 +0200

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,40 +4,33 @@ const handlePostback = async (event, pageAccessToken) => {
   const { id: senderId } = event.sender || {};
   const { payload } = event.postback || {};
 
-const getStarted = async (send) => send({
-  attachment: {
+  const introduction = "Welcome to Standby Clothing AI Assistance!\nI'm your virtual stylist, here to help you with all your fashion needs.\n\nFor a seamless experience, I recommend interacting with me on Messenger.\n\nPlease note that Standby Clothing is proudly owned by Njabulo Hlatshwayo.\n\nHow May I Be Of Service Today ?";
+  const attachment = {
     type: "template",
     payload: {
       template_type: "button",
-      text: api.introduction,
+      text: introduction,
       buttons: [
         {
           type: "postback",
-          title: "Commands",
-          payload: "HELP"
-        },
-        {
-          type: "postback",
-          title: "About",
+          title: "About us",
           payload: "ABOUT"
         },
         {
           type: "postback",
-          title: "Prefix",
-          payload: "PREFIX"
+          title: "Products",
+          payload: "CLOTHES"
         }
       ]
     }
-  }
-});
-   const send = async text => sendMessage(senderID, typeof text === "object" ? text : { text }, pageAccessToken);
+  };
+
 
   if (!senderId || !payload) return console.error('Invalid postback event object');
   if (payload === "postback_welcome") {
-     await getStarted(send);
+     return await sendMessage(senderId, { attachment }, pageAccessToken);
   };
   try {
-
     await sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
   } catch (err) {
     console.error('Error sending postback response:', err.message || err);
