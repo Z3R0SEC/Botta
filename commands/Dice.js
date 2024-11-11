3c7ac62f9da430fb04a02772db394adc357e29ad
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-11-11 20:11:10 +0200

    Create Dice.js

---
+++ commands/Dice.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'dice',
+  description: 'Roll a dice',
+  usage: 'diceroll',
+  author: 'Thabani',
+
+  async execute(senderId, args, pageAccessToken) {
+    const result = Math.floor(Math.random() * 6) + 1;
+    sendMessage(senderId, { text: `[»] Roll: ${result}!` }, pageAccessToken);
+  }
+};
