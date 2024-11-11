46639f3cd619cbbda4e22c909b01d6d93d1a96bc
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-11-11 20:31:48 +0200

    Update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,10 +1,12 @@
+const { sendMessage } = require('../handles/sendMessage');
+
 module.exports = {
   name: 'uid',
   description: 'Retrieve the user’s unique Facebook ID',
   usage: 'uid',
   author: 'coffee',
 
-  async execute(senderId, sendMessage, args, pageAccessToken) {
+  async execute(senderId, args, pageAccessToken) {
     sendMessage(senderId, { text: `${senderId}` }, pageAccessToken);
   }
 };
