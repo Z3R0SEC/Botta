5a90a6280d421ae73bb934e41db4877a50d3f80f
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-11-11 20:31:08 +0200

    Update Dice.js

--- commands/Dice.js
+++ commands/Dice.js
@@ -1,10 +1,12 @@
+const { sendMessage } = require('../handles/sendMessage');
+
 module.exports = {
   name: 'dice',
   description: 'Roll a dice',
   usage: 'diceroll',
   author: 'Thabani',
 
-  async execute(senderId, sendMessage, args, pageAccessToken) {
+  async execute(senderId, args, pageAccessToken) {
     const result = Math.floor(Math.random() * 6) + 1;
     sendMessage(senderId, { text: `[»] Roll: ${result}!` }, pageAccessToken);
   }
