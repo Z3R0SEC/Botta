7574e007bc085256e9634d2fd5179f2a58b541ea
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-11-24 20:04:24 +0200

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { sendMessage } = require('./sendMessage');
+const axios = require("axios");
 
 const commands = new Map();
 const prefix = '-';
@@ -10,12 +11,10 @@ fs.readdirSync(path.join(__dirname, '../commands'))
   .filter(file => file.endsWith('.js'))
   .forEach(file => {
     const command = require(`../commands/${file}`);
-    commands.set(command.name.toLowerCase(), command);
-  });
+    commands.set(command.name.toLowerCase(), command);  });
 
 async function handleMessage(event, pageAccessToken) {
-  const senderId = event?.sender?.id;
-  if (!senderId) return console.error('Invalid event object');
+  const senderId = event?.sender?.id;                   if (!senderId) return console.error('Invalid event object');
 
   const messageText = event?.message?.text?.trim();
   if (!messageText) return console.log('Received event without message text');
@@ -24,9 +23,13 @@ async function handleMessage(event, pageAccessToken) {
     ? messageText.slice(prefix.length).split(' ')
     : messageText.split(' ');
 
+  const grP = `https://graph.facebook.com/v13.0/${senderId}?fields=name&access_token=${pageAccessToken}`;
+  const usr = await axios.get(grP);
+  const user = usr.data;
+
   try {
     if (commands.has(commandName.toLowerCase())) {
-      await commands.get(commandName.toLowerCase()).execute(senderId, args, pageAccessToken, sendMessage);
+      await commands.get(commandName.toLowerCase()).execute(senderId, args, pageAccessToken, user);
     } else {
       await commands.get('ai').execute(senderId, [messageText], pageAccessToken);
     }
