d0501a389655677a1e47e376a5b11020ae725e6a
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-11-24 20:30:53 +0200

    Delete commands/zold directory

--- commands/zold/test.py
+++
@@ -1,7 +0,0 @@
-from requests import get, post
-
-api = "https://raaj-api.x10.bz/ai"
-
-req = get(api, params={ "user": "Msomi", "prompt": "Nothing Much Buddy Please Memorize This Code ill Ask forbit now: 3458"})
-
-print(req.json())
