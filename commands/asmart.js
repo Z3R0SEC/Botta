76ee125b961609bc8841b23c6bbc61f18c903fbd
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-02-03 08:33:20 +0200

    Update asmart.js

--- commands/asmart.js
+++ commands/asmart.js
@@ -27,7 +27,7 @@ module.exports = {
          user: id,
          prompt: prompt
       };
-      const apiUrl = "https://mota-api.x10.bz/ai";
+      const apiUrl = "https://mota-api.x10.bz/api/ai";
 
       try {
          const response = await axios.get(apiUrl, { params: apiParams });
