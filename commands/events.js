c4b164051bf7123b98d24f58ab8704dbd897acc4
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-02-12 19:21:42 +0200

    Create events.js

---
+++ commands/events.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'event',
+  description: 'Event',
+  usage: 'Fetch bot Event',
+  author: 'me',
+
+  async execute(senderId, args, pageAccessToken, event) {
+  await sendMessage(senderId, { text: event }, pageAccessToken);
+  };
